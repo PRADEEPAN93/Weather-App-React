85a35e977a724712629c20de2419f0b06d814c99
PRADEEPAN93 <pradeepank6070@gmail.com>
2025-11-17 15:54:24 +0530

    error slove

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Weather-App-React/",  // 👈 your repo name
+  base: "/Weather-App-React/",
 });
